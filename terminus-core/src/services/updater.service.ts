90c0eadfb9f9ebf3308d445ddec31e9d44d8bf2c
Vyacheslav Nosal <buper0719@gmail.com>
2019-06-04 05:21:43 +0000

    Bump xterm from 3.14.1 to 3.14.2 in /terminus-terminal

--- terminus-core/src/services/updater.service.ts
+++ terminus-core/src/services/updater.service.ts
@@ -36,7 +36,7 @@ export class UpdaterService {
 
         this.logger.debug('Checking for updates')
 
-        if (this.electronUpdaterAvailable && !process.env.TERMINUS_DEV) {
+        if (this.electronUpdaterAvailable) {
             try {
                 this.autoUpdater.checkForUpdates()
             } catch (e) {
