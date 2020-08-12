61eadca736081b1fba3329969bcf8dfa730709c9
Vyacheslav Nosal <buper0719@gmail.com>
2020-08-12 21:04:42 +0200

    Update xtermFrontend.ts

--- terminus-terminal/src/frontends/xtermFrontend.ts
+++ terminus-terminal/src/frontends/xtermFrontend.ts
@@ -110,7 +110,9 @@ export class XTermFrontend extends Frontend {
                     let cols = Math.floor(l / actualCellWidth)
                     let rows = Math.floor(i / actualCellHeight)
 
-                    this.xterm.resize(cols, rows)
+                    if (!isNaN(cols) && !isNaN(rows)) {
+                        this.xterm.resize(cols, rows)
+                    }
                 }
             } catch (e) {
                 // tends to throw when element wasn't shown yet
