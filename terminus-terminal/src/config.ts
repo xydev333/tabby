fd53114d6ec5cc1d4d80652d5426d703f72df869
Vyacheslav Nosal <buper0719@gmail.com>
2020-10-14 15:43:12 +0800

    feat: Add copy button to ssh connection

--- terminus-terminal/src/config.ts
+++ terminus-terminal/src/config.ts
@@ -65,7 +65,6 @@ export class TerminalConfigProvider extends ConfigProvider {
             recoverTabs: true,
             warnOnMultilinePaste: true,
             showDefaultProfiles: true,
-            searchRegexAlwaysEnabled: false,
         },
     }
 
